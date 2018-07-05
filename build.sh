eab811c342315de29e2b4d580f419bd9ba96da2f
Sylvain Hallé <shalle@acm.org>
2018-07-05 09:56:03 -0400

    Fix to build script

--- build.sh
+++ build.sh
@@ -4,11 +4,21 @@
 # (C) 2018 Sylvain Hallé
 
 PKG_FOLDER=contents
+BRANCH_NAME=dev
+
+# Create folders
+mkdir -p $PKG_FOLDER/opt/textidote
+mkdir -p $PKG_FOLDER/usr/local/bin
+mkdir -p $PKG_FOLDER/usr/share/man/man1
+mkdir -p $PKG_FOLDER/etc/bash.completion.d
 
 # Fetch JAR and other files from master branch
-git show master:textidote.jar > $PKG_FOLDER/opt/textidote/textidote.jar
-#git show master:Completions/textidote.zsh > $PKG_FOLDER/opt/textidote/textidote.zsh
-#git show master:Completions/textidote.bash > $PKG_FOLDER/etc/bash.completion.d/textidote
+if [ ! -f $PKG_FOLDER/opt/textidote/textidote.jar ]; then
+	echo "Downloading latest release of textidote.jar"
+	wget https://github.com/sylvainhalle/textidote/releases/download/v0.4/textidote.jar -P $PKG_FOLDER/opt/textidote
+fi
+git show $BRANCH_NAME:Completions/textidote.zsh > $PKG_FOLDER/opt/textidote/textidote.zsh
+git show $BRANCH_NAME:Completions/textidote.bash > $PKG_FOLDER/etc/bash.completion.d/textidote
 
 # Create MD5 checksums
 pushd $PKG_FOLDER
