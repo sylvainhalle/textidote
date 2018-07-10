b2e64eee8404ae4eeca26cfa287ec2cc67a9201b
Sylvain Hallé <shalle@acm.org>
2018-07-10 09:10:37 -0400

    Increased version number

--- Source/Core/src/ca/uqac/lif/textidote/Main.java
+++ Source/Core/src/ca/uqac/lif/textidote/Main.java
@@ -38,7 +38,6 @@ import ca.uqac.lif.textidote.cleaning.CompositeCleaner;
 import ca.uqac.lif.textidote.cleaning.ReplacementCleaner;
 import ca.uqac.lif.textidote.cleaning.TextCleanerException;
 import ca.uqac.lif.textidote.cleaning.latex.LatexCleaner;
-import ca.uqac.lif.textidote.cleaning.markdown.MarkdownCleaner;
 import ca.uqac.lif.textidote.render.AnsiAdviceRenderer;
 import ca.uqac.lif.textidote.render.HtmlAdviceRenderer;
 import ca.uqac.lif.textidote.rules.CheckCaptions;
@@ -59,7 +58,7 @@ import ca.uqac.lif.util.NullPrintStream;
 
 /**
  * Command-line interface for TeXtidote.
- * @author Sylvain Hallér
+ * @author Sylvain Hallé
  */
 public class Main 
 {
@@ -76,19 +75,12 @@ public class Main
 	/**
 	 * A version string
 	 */
-	protected static final String VERSION_STRING = "0.6";
+	protected static final String VERSION_STRING = "0.5.1";
 
 	/**
-	 * The name of the Aspell dictionary file to look for in a folder. The
-	 * "XX" must be there, as it is replaced with an actual language code
-	 * at runtim.
+	 * The name of the Aspell dictionary file to look for in a folder
 	 */
 	protected static final String ASPELL_DICT_FILENAME = ".aspell.XX.pws";
-
-	/**
-	 * The name of the optional file containing command line parameters
-	 */
-	protected static final String PARAM_FILENAME = ".textidote";
 
 	/**
 	 * Main method. This method simply calls the static method
@@ -111,58 +103,22 @@ public class Main
 	 */
 	public static int mainLoop(String[] args, InputStream in, PrintStream out, PrintStream err) throws IOException
 	{
-		// Store input type
-		Linter.Language input_type = Linter.Language.UNSPECIFIED;
-
 		// Setup command line parser and arguents
 		CliParser cli_parser = new CliParser();
-		cli_parser.addArgument(new Argument().withLongName("check").withArgument("lang").withDescription("Checks grammar in language lang"));
-		cli_parser.addArgument(new Argument().withLongName("clean").withDescription("Remove markup from input file"));
-		cli_parser.addArgument(new Argument().withLongName("dict").withArgument("file").withDescription("Load dictionary from file"));
-		cli_parser.addArgument(new Argument().withLongName("help").withDescription("\tShow command line usage"));
 		cli_parser.addArgument(new Argument().withLongName("html").withDescription("\tFormats the report as HTML"));
-		cli_parser.addArgument(new Argument().withLongName("ignore").withArgument("rules").withDescription("Ignore rules"));
-		cli_parser.addArgument(new Argument().withLongName("map").withArgument("file").withDescription("Output correspondence map to file"));
-		cli_parser.addArgument(new Argument().withLongName("name").withArgument("n").withDescription("Use n as app name when printing usage"));
 		cli_parser.addArgument(new Argument().withLongName("no-color").withDescription("Disables colors in ANSI printing"));
-		cli_parser.addArgument(new Argument().withLongName("no-config").withDescription("Ignore config file if any"));
-		cli_parser.addArgument(new Argument().withLongName("quiet").withDescription("Don't print any message"));
+		cli_parser.addArgument(new Argument().withLongName("check").withArgument("lang").withDescription("Checks grammar in language lang"));
+		cli_parser.addArgument(new Argument().withLongName("ignore").withArgument("rules").withDescription("Ignore rules"));
+		cli_parser.addArgument(new Argument().withLongName("dict").withArgument("file").withDescription("Load dictionary from file"));
+		cli_parser.addArgument(new Argument().withLongName("detex").withDescription("Detex input file"));
+		cli_parser.addArgument(new Argument().withLongName("map").withArgument("file").withDescription("Output correspondence map to file"));
 		cli_parser.addArgument(new Argument().withLongName("read-all").withDescription("Don't ignore lines before \\begin{document}"));
 		cli_parser.addArgument(new Argument().withLongName("replace").withArgument("file").withDescription("Apply replacement patterns from file"));
-		cli_parser.addArgument(new Argument().withLongName("type").withArgument("x").withDescription("Input is of type x (tex or md)"));
+		cli_parser.addArgument(new Argument().withLongName("quiet").withDescription("Don't print any message"));
+		cli_parser.addArgument(new Argument().withLongName("help").withDescription("\tShow command line usage"));
 		cli_parser.addArgument(new Argument().withLongName("version").withDescription("Show version number"));
-
-		// Check if there is a parameter filename
-		ArgumentMap map = null;
-		File param_file = new File(PARAM_FILENAME);
-		if (param_file.exists())
-		{
-			Scanner f_c_scan = new Scanner(param_file);
-			String[] f_c_args = readArguments(f_c_scan);
-			map = cli_parser.parse(f_c_args);
-		}
-		ArgumentMap map_cline = cli_parser.parse(args);
-		if (map == null)
-		{
-			// If no arguments were read from a file, just use those from
-			// the actual command line
-			map = map_cline;
-		}
-		else
-		{
-			if (map_cline.hasOption("no-config"))
-			{
-				// Just use command line args
-				map = map_cline;
-			}
-			else
-			{
-				// Merge CLI and args from file
-				map.putAll(map_cline);
-			}
-		}
-
-		// Process command line arguments
+		cli_parser.addArgument(new Argument().withLongName("name").withArgument("n").withDescription("Use n as app name when printing usage"));
+		ArgumentMap map = cli_parser.parse(args);
 		String app_name = "java -jar textidote.jar";
 		if (map == null)
 		{
@@ -216,25 +172,9 @@ public class Main
 			stdout.close();
 			return 0;
 		}
-		if (map.hasOption("type"))
-		{
-			String type = map.getOptionValue("type");
-			if (type.compareToIgnoreCase("md") == 0)
-			{
-				input_type = Linter.Language.MARKDOWN;
-			}
-			else if (type.compareToIgnoreCase("tex") == 0)
-			{
-				input_type = Linter.Language.LATEX;
-			}
-			else if (type.compareToIgnoreCase("txt") == 0)
-			{
-				input_type = Linter.Language.TEXT;
-			}
-		}
 
 		// Only detex input
-		if (map.hasOption("clean"))
+		if (map.hasOption("detex"))
 		{
 			CompositeCleaner cleaner = new CompositeCleaner();
 			if (map.hasOption("replace"))
@@ -252,6 +192,9 @@ public class Main
 					stderr.println("Using replacement file " + replacement_filename);
 				}
 			}
+			LatexCleaner latex_cleaner = new LatexCleaner();
+			latex_cleaner.setIgnoreBeforeDocument(!read_all);
+			cleaner.add(latex_cleaner);
 			List<String> filenames = map.getOthers();
 			if (filenames.isEmpty())
 			{
@@ -277,26 +220,9 @@ public class Main
 					{
 						scanner = new Scanner(f);
 					}
-<<<<<<< HEAD
-					// Create cleaner based on file extension
-					CompositeCleaner c_file = new CompositeCleaner(cleaner);
-					if (input_type == Linter.Language.LATEX || (filename.compareTo("--") == 0 && input_type == Linter.Language.UNSPECIFIED) || filename.endsWith(".tex"))
-					{
-						// LaTeX file
-						LatexCleaner latex_cleaner = new LatexCleaner();
-						latex_cleaner.setIgnoreBeforeDocument(!read_all);
-						c_file.add(latex_cleaner);
-					}
-					else if (input_type == Linter.Language.MARKDOWN || filename.endsWith(".md"))
-					{
-						MarkdownCleaner markdown_cleaner = new MarkdownCleaner();
-						c_file.add(markdown_cleaner);
-					}
-=======
->>>>>>> More unit tests
 					AnnotatedString s = AnnotatedString.read(scanner);
 					s.setResourceName(filename);
-					AnnotatedString ds = c_file.clean(s);
+					AnnotatedString ds = cleaner.clean(s);
 					stdout.println(ds);
 					if (map.hasOption("map"))
 					{
@@ -346,14 +272,19 @@ public class Main
 				stderr.println("Using replacement file " + replacement_filename);
 			}
 		}
+		LatexCleaner latex_cleaner = new LatexCleaner();
+		latex_cleaner.setIgnoreBeforeDocument(!read_all);
+		cleaner.add(latex_cleaner);
+		Linter linter = new Linter(cleaner);
+		populateRules(linter);
+		linter.addToBlacklist(rule_blacklist);
 
 		// Do we check the language?
-		List<String> dictionary = new ArrayList<String>();
-		String lang_s = "";
 		if (map.hasOption("check"))
 		{
-			lang_s = map.getOptionValue("check");
+			String lang_s = map.getOptionValue("check");
 			// Try to read dictionary from an Aspell file
+			List<String> dictionary = new ArrayList<String>();
 			try
 			{
 				String dict_filename = ASPELL_DICT_FILENAME.replace("XX", lang_s);
@@ -376,6 +307,16 @@ public class Main
 				{
 					stderr.println("Dictionary not found: " + map.getOptionValue("dict"));
 				}
+			}
+			try
+			{
+				linter.addDetexed(new CheckLanguage(LanguageFactory.getLanguageFromString(lang_s), dictionary));
+			}
+			catch (CheckLanguage.UnsupportedLanguageException e)
+			{
+				stderr.println("Unknown language: " + map.getOptionValue("check"));
+				stdout.close();
+				return -1;
 			}
 		}
 
@@ -405,40 +346,8 @@ public class Main
 						stderr.println("File " + filename + " not found (skipping)");
 						continue;
 					}
+
 					scanner = new Scanner(f);
-				}
-				CompositeCleaner c_cleaner = new CompositeCleaner(cleaner);
-				Linter linter = null;
-				if (input_type == Linter.Language.MARKDOWN || filename.endsWith(".md"))
-				{
-					MarkdownCleaner markdown_cleaner = new MarkdownCleaner();
-					linter = new Linter(c_cleaner);
-					c_cleaner.add(markdown_cleaner);
-					linter = new Linter(c_cleaner);
-					populateMarkdownRules(linter);
-					linter.addToBlacklist(rule_blacklist);
-				}
-				else
-				{
-					LatexCleaner latex_cleaner = new LatexCleaner();
-					latex_cleaner.setIgnoreBeforeDocument(!read_all);
-					c_cleaner.add(latex_cleaner);
-					linter = new Linter(c_cleaner);
-					populateLatexRules(linter);
-					linter.addToBlacklist(rule_blacklist);
-				}
-				if (!lang_s.isEmpty())
-				{
-					try
-					{
-						linter.addCleaned(new CheckLanguage(LanguageFactory.getLanguageFromString(lang_s), dictionary));
-					}
-					catch (CheckLanguage.UnsupportedLanguageException e)
-					{
-						stderr.println("Unknown language: " + map.getOptionValue("check"));
-						stdout.close();
-						return -1;
-					}
 				}
 				last_string = processDocument(scanner, filename, linter, all_advice);
 			}
@@ -501,19 +410,19 @@ public class Main
 	 */
 	protected static void printGreeting(AnsiPrinter out)
 	{
-		out.println("TeXtidote v" + VERSION_STRING + " - A linter for LaTeX documents and others");
+		out.println("TeXtidote v" + VERSION_STRING + " - A linter for LaTeX documents");
 		out.println("(C) 2018 Sylvain Hallé - All rights reserved");
 		out.println();
 	}
 
 	/**
-	 * Adds the rules to the LaTeX linter
+	 * Adds the rules to the linter
 	 * @param linter The linter to configure
 	 */
-	protected static void populateLatexRules(Linter linter)
+	protected static void populateRules(Linter linter)
 	{
 		linter.add(readRules(REGEX_FILENAME).values());
-		linter.addCleaned(readRules(REGEX_FILENAME_DETEX).values());
+		linter.addDetexed(readRules(REGEX_FILENAME_DETEX).values());
 		linter.add(new CheckFigureReferences());
 		linter.add(new CheckFigurePaths());
 		linter.add(new CheckCaptions());
@@ -521,15 +430,6 @@ public class Main
 		linter.add(new CheckSubsectionSize());
 		linter.add(new CheckNoBreak());
 		linter.add(new CheckCiteMix());
-	}
-	
-	/**
-	 * Adds the rules to the Markdown linter
-	 * @param linter The linter to configure
-	 */
-	protected static void populateMarkdownRules(Linter linter)
-	{
-		// Do nothing
 	}
 
 	/**
@@ -626,62 +526,5 @@ public class Main
 			ps.print(map.get(key));
 			ps.println();
 		}
-	}
-
-	/**
-	 * Reads command-line arguments from a file.
-	 * @param scanner A scanner open on the file to read from
-	 * @return An array of strings, similar to what would be in the
-	 * <tt>args</tt> input of the {@link #main(String[])} method if the
-	 * arguments were received from the command line.
-	 */
-	/* This method has protected visibility in order to 
-	 * be accessible from the unit tests */
-	/*@ non_null @*/ static String[] readArguments(/*@ non_null @*/ Scanner scanner)
-	{
-		List<String> arguments = new ArrayList<String>();
-		StringBuilder current_argument = new StringBuilder();
-		while (scanner.hasNextLine())
-		{
-			String line = scanner.nextLine().trim();
-			if (line.isEmpty() || line.startsWith("#"))
-			{
-				// Blank and "comment" lines are ignored
-				continue;
-			}
-			String[] parts = line.split("\\s+");
-			boolean quotes = false;
-			for (String part : parts)
-			{
-				part = part.trim();
-				if (part.startsWith(("\"")))
-				{
-					quotes = true;
-					part = part.substring(1);
-				}
-				current_argument.append(part).append(" ");
-				if (part.endsWith("\""))
-				{
-					quotes = false;
-				}
-				if (!quotes)
-				{
-					String arg = current_argument.toString().trim();
-					if (arg.endsWith("\""))
-					{
-						arg = arg.substring(0, arg.length() - 1);
-					}
-					arguments.add(arg);
-					current_argument = new StringBuilder();
-				}
-			}
-		}
-		scanner.close();
-		String[] out = new String[arguments.size()];
-		for (int i = 0; i < arguments.size(); i++)
-		{
-			out[i] = arguments.get(i);
-		}
-		return out;
 	}
 }
