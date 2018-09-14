5333f423d31175369b77180acd7d35888bfbdac9
Sylvain Hallé <shalle@acm.org>
2018-09-14 08:11:37 -0400

    Change to gitignore

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ if [ ! -f $PKG_FOLDER/opt/textidote/textidote.jar ]; then
 	echo "Downloading latest release of textidote.jar"
 	wget https://github.com/sylvainhalle/textidote/releases/download/v0.4/textidote.jar -P $PKG_FOLDER/opt/textidote
 fi
-git show $BRANCH_NAME:Completions/textidote.zsh > $PKG_FOLDER/opt/textidote/textidote.zsh
-git show $BRANCH_NAME:Completions/textidote.bash > $PKG_FOLDER/etc/bash.completion.d/textidote
+#git show $BRANCH_NAME:Completions/textidote.zsh > $PKG_FOLDER/opt/textidote/textidote.zsh
+#git show $BRANCH_NAME:Completions/textidote.bash > $PKG_FOLDER/etc/bash.completion.d/textidote
 
 # Create MD5 checksums
 pushd $PKG_FOLDER
