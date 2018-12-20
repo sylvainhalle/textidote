66037960bc1da05a9da882a3b9941bab1f512bd8
Sylvain Hallé <shalle@acm.org>
2018-12-20 19:19:50 +0100

    Closes #28

--- Source/Core/src/ca/uqac/lif/textidote/cleaning/latex/LatexCleaner.java
+++ Source/Core/src/ca/uqac/lif/textidote/cleaning/latex/LatexCleaner.java
@@ -193,7 +193,7 @@ public class LatexCleaner extends TextCleaner
 		as_out = as_out.replaceAll("\\\\oe\\{\\}", "œ");
 		as_out = as_out.replaceAll("\\\\ae\\{\\}", "æ");
 		// Common environments
-		as_out = as_out.replaceAll("\\\\(begin|end)\\{(itemize|enumerate|inparaenum|document|thm|abstract|eqnarray|compactitem|query|center|minipage)\\}", "");
+		as_out = as_out.replaceAll("\\\\(begin|end)\\{(itemize|enumerate|inparaenum|document|thm|abstract|eqnarray|compactitem|query|center|minipage|quote)\\}", "");
 		// List items
 		as_out = as_out.replaceAll("\\\\item\\s*", "");
 		// Images
