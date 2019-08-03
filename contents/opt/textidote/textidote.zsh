10393fbccd658c7818857f255cbe09efb2340d9b
Sylvain Hallé <shalle@acm.org>
2019-08-03 08:12:22 -0400

    Closes #65

--- contents/opt/textidote/textidote.zsh
+++ contents/opt/textidote/textidote.zsh
@@ -8,7 +8,7 @@
 # ----------------------------------------------------------------------
 _textidote_complete()
 {
-	_arguments '--check[Check grammar]:lang:->lang' '--detex[Clear markup]' '--dict[Use dictionary]:filename:_files' '--help[Show command line usage]' '--version[Show version]' '--output[Select output format]:format:->format' '--map[Produce map file]:filename:_files' '--no-color[No ANSI color]' '--no-config[Ignore config file]' '--quiet[No messages]' '--read-all[Read all file]' '--replace[Apply replacements]:filename:_files' '--type[Set input type]:type:->type'
+	_arguments '--check[Check grammar]:lang:->lang' '--clean[Clear markup]' '--dict[Use dictionary]:filename:_files' '--help[Show command line usage]' '--version[Show version]' '--output[Select output format]:format:->format' '--map[Produce map file]:filename:_files' '--no-color[No ANSI color]' '--no-config[Ignore config file]' '--quiet[No messages]' '--read-all[Read all file]' '--replace[Apply replacements]:filename:_files' '--type[Set input type]:type:->type'
 	case "$state" in
 	lang)
 		_values -s ' ' 'lang' de de_AT de_CH en en_CA en_UK es fr nl pt
