4addaf8a6d988e55f772a9e84608ebb4f7904f3c
Sylvain Hallé <shalle@acm.org>
2019-08-05 15:28:39 -0400

    Updated completions

--- contents/opt/textidote/textidote.zsh
+++ contents/opt/textidote/textidote.zsh
@@ -8,13 +8,13 @@
 # ----------------------------------------------------------------------
 _textidote_complete()
 {
-	_arguments '--check[Check grammar]:lang:->lang' '--clean[Clear markup]' '--dict[Use dictionary]:filename:_files' '--help[Show command line usage]' '--version[Show version]' '--output[Select output format]:format:->format' '--map[Produce map file]:filename:_files' '--no-color[No ANSI color]' '--no-config[Ignore config file]' '--quiet[No messages]' '--read-all[Read all file]' '--replace[Apply replacements]:filename:_files' '--type[Set input type]:type:->type'
+	_arguments '--check[Check grammar]:lang:->lang' '--clean[Clear markup]' '--dict[Use dictionary]:filename:_files' '--help[Show command line usage]' '--ignore[Ignore rules]' '--languagemodel[Use n-grams]' '--map[Produce map file]:filename:_files' '--version[Show version]' '--no-color[No ANSI color]' '--no-config[Ignore config file]' '--quiet[No messages]' '--read-all[Read all file]' '--remove[Remove environments]' '--remove-macros[Remove macros]' '--replace[Apply replacements]:filename:_files' '--type[Set input type]:type:->type' '--version[Show version]' '--output[Select output format]:format:->format'
 	case "$state" in
 	lang)
 		_values -s ' ' 'lang' de de_AT de_CH en en_CA en_UK es fr nl pt
 		;;
 	format)
-		_values -s ' ' 'format' html plain singleline
+		_values -s ' ' 'format' json html plain singleline
 		;;
 	type)
 		_values -s ' ' 'type' md tex
