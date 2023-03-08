f08a969d1bf01af53700d5ee23e83dae490964af
Sylvain Hallé <shalle@acm.org>
2023-03-08 17:15:16 -0500

    Re-upgrade to version number

--- Source/Core/src/ca/uqac/lif/textidote/Main.java
+++ Source/Core/src/ca/uqac/lif/textidote/Main.java
@@ -82,7 +82,7 @@ public class Main
 	/**
 	 * A version string
 	 */
-	public static final String VERSION_STRING = "0.8.4";
+	public static final String VERSION_STRING = "0.9";
 
 	/**
 	 * The name of the Aspell dictionary file to look for in a folder
